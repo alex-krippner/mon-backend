80d2b8939d6735c64da413c4913103237e7fb65e
Alex Krippner <alexakrippner@gmail.com>
2022-06-16 23:20:20 +0200

    MON-25: Add handling of incomplete data

---
+++ storage/utils.go
@@ -0,0 +1,16 @@
+package storage
+
+import "encoding/json"
+
+type VocabStructs interface {
+	*VocabDefinitions | *VocabSentences | *PartsOfSpeech
+}
+
+type KanjiStructs interface {
+	*ExampleSentences | *ExampleWords
+}
+
+func HandleNil[S VocabStructs | KanjiStructs](s S) error {
+	b := []byte(`[]`)
+	return json.Unmarshal(b, &s)
+}
