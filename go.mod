907f17fb9458a1dfa3694544fc7529b27aff099e
Alex Krippner <alexakrippner@gmail.com>
2023-06-03 19:00:25 +0200

    MK-33: Remove urfave CLI package

--- go.mod
+++ go.mod
@@ -3,14 +3,9 @@ module mon-backend
 go 1.18
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.10.5
-	github.com/urfave/cli/v2 v2.4.0
-	github.com/gorilla/handlers v1.5.1
 )
 
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/felixge/httpsnoop v1.0.1 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-)
+require github.com/felixge/httpsnoop v1.0.1 // indirect
--- main.go
+++ main.go
@@ -8,63 +8,40 @@ import (
 
 	"mon-backend/apiserver"
 	"mon-backend/storage"
-
-	"github.com/urfave/cli/v2"
 )
 
 const (
-	apiServerAddrFlagName       string = "addr"
-	apiServerStorageDatabaseURL string = "database-url"
+	apiServerAddrFlagName       string = "API_SERVER_ADDR"
+	apiServerStorageDatabaseURL string = "DATABASE_URL"
 )
 
 func main() {
-	if err := app().Run(os.Args); err != nil {
+	if err := startServer(); err != nil {
 		fmt.Println("could not run application", err)
 	}
 }
 
-func app() *cli.App {
-	return &cli.App{
-		Name:  "mon-api-server",
-		Usage: "The API",
-		Commands: []*cli.Command{
-			apiServerCmd(),
-		},
+func startServer() error {
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	// Noob comment: empty struct requires no memory
+	stopper := make(chan struct{})
+	go func() {
+		// Noob Comment: Block here until a value is received by the channel "done"
+		<-done
+		close(stopper)
+	}()
+	databaseURL := os.Getenv(apiServerStorageDatabaseURL)
+	s, err := storage.NewStorage(databaseURL)
+	if err != nil {
+		return fmt.Errorf("could not initialize storage: %w", err)
 	}
-}
 
-func apiServerCmd() *cli.Command {
-	return &cli.Command{
-		Name:  "start",
-		Usage: "starts the API server",
-		Flags: []cli.Flag{
-			&cli.StringFlag{Name: apiServerAddrFlagName, EnvVars: []string{"API_SERVER_ADDR"}},
-			&cli.StringFlag{Name: apiServerStorageDatabaseURL, EnvVars: []string{"DATABASE_URL"}},
-		},
-		Action: func(c *cli.Context) error {
-			done := make(chan os.Signal, 1)
-			signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-			// Noob comment: empty struct requires no memory
-			stopper := make(chan struct{})
-			go func() {
-				// Noob Comment: Block here until a value is received by the channel "done"
-				<-done
-				close(stopper)
-			}()
-
-			databaseURL := c.String(apiServerStorageDatabaseURL)
-			s, err := storage.NewStorage(databaseURL)
-			if err != nil {
-				return fmt.Errorf("could not initialize storage: %w", err)
-			}
-
-			addr := c.String(apiServerAddrFlagName)
-			server, err := apiserver.NewAPIServer(addr, s)
-			if err != nil {
-				return err
-			}
-
-			return server.Start(stopper)
-		},
+	addr := os.Getenv(apiServerAddrFlagName)
+	server, err := apiserver.NewAPIServer(addr, s)
+	if err != nil {
+		return err
 	}
+
+	return server.Start(stopper)
 }
