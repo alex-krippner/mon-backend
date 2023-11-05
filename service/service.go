c42000e75524686e1d38f479b5657ab635bbc04e
Alex Krippner <alexakrippner@gmail.com>
2023-11-05 14:59:20 +0100

    MK-34: Add service package

---
+++ service/service.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"context"
+	"mon-backend/adapters"
+	"mon-backend/app"
+	"mon-backend/app/handler"
+)
+
+func NewApplication(ctx context.Context) app.Application {
+	return newApplication(ctx)
+}
+
+func newApplication(ctx context.Context) app.Application {
+	db, err := adapters.GetDatabase()
+	if err != nil {
+		panic(err)
+	}
+	repositories := adapters.InitRepositories(db)
+
+	return app.Application{
+		Handlers: app.Handlers{
+			ReadingHandler: handler.NewReadingHandler(repositories.ReadingRepository),
+		},
+	}
+}
