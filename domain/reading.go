de7f79991e51ce1bafa12bfb5b0619a693aafcc6
Alex Krippner <alexakrippner@gmail.com>
2023-11-05 14:56:30 +0100

    MK-34: Add reading domain

---
+++ domain/reading.go
@@ -0,0 +1,41 @@
+package domain
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type Reading struct {
+	ID          string `json:"id,omitempty"`
+	Translation string `json:"translation,omitempty"`
+	Japanese    string `json:"japanese,omitempty"`
+	Title       string `json:"title,omitempty"`
+}
+
+type ReadingRepository interface {
+	CreateReading(ctx context.Context, req *Reading) (*Reading, error)
+	GetAllReading(ctx context.Context) ([]*Reading, error)
+	UpdateReading(ctx context.Context, r *Reading) (*Reading, error)
+	DeleteReading(id string) error
+}
+
+func NewReading(translation string, japanese string, title string) (*Reading, error) {
+	if translation == "" {
+		return nil, errors.New("reading translation missing")
+	}
+	if japanese == "" {
+		return nil, errors.New("japanese text missing")
+	}
+	if title == "" {
+		return nil, errors.New("reading title is missing")
+	}
+
+	return &Reading{
+		ID:          uuid.New().String(),
+		Translation: translation,
+		Japanese:    japanese,
+		Title:       title,
+	}, nil
+}
