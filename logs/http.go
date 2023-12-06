c5eb8c1d2e394ff0a65644079e0231051cc2e88a
alex-krippner <52562506+alex-krippner@users.noreply.github.com>
2023-12-06 21:59:13 +0100

    Revert "MK-34: Add factory function for logger"
    This reverts commit 7b59c002f7fd821b6a7028ee0a3ed1c2e8d32954.

--- logs/http.go
+++ logs/http.go
@@ -9,35 +9,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func NewStructuredLogger(logger *logrus.Logger) func(next http.Handler) http.Handler {
-	return middleware.RequestLogger(&StructuredLogger{logger})
-}
-
-// based on example from chi: https://github.com/go-chi/chi/blob/master/_examples/logging/main.go
-type StructuredLogger struct {
-	Logger *logrus.Logger
-}
-
-func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
-	entry := &StructuredLoggerEntry{Logger: logrus.NewEntry(l.Logger)}
-	logFields := logrus.Fields{}
-
-	if reqID := middleware.GetReqID(r.Context()); reqID != "" {
-		logFields["req_id"] = reqID
-	}
-
-	logFields["http_method"] = r.Method
-
-	logFields["remote_addr"] = r.RemoteAddr
-	logFields["uri"] = r.RequestURI
-
-	entry.Logger = entry.Logger.WithFields(logFields)
-
-	entry.Logger.Info("Request started")
-
-	return entry
-}
-
 type StructuredLoggerEntry struct {
 	Logger logrus.FieldLogger
 }
@@ -48,13 +19,13 @@ func (l *StructuredLoggerEntry) Write(status, bytes int, header http.Header, ela
 		"resp_elapsed": elapsed.Round(time.Millisecond / 100).String(),
 	})
 
-	l.Logger.Info("Request completed	")
+	l.Logger.Info("Request completed")
 }
 
 func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
 	l.Logger = l.Logger.WithFields(logrus.Fields{
 		"stack": string(stack),
-		"panic": fmt.Sprintf("%+v", v),
+		"panic": fmt.Sprint("%+v", v),
 	})
 }
 
